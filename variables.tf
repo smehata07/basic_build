74e2dfe9a4cb5120820b0ecf9028702ac8fb49e4
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 13:46:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -17,7 +17,7 @@ variable "virtual_machines" {
     "vm1" = {
       server_name = "sonu_vm_1_object"
       machine_type = "e2-micro"
-      server_zone = "us-west4-b"
+      server_zone = "asia-east1-b"
       vpc_name = "sonu_vpc_1_object"
       static_ip = "10.0.0.1"
       subnet_name = "sonu_subnet_1_object"
