b51d24dff5d2fda1610c72dfd10c6722c65d9233
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 15:44:21 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,28 +1,38 @@
 variable "vpc" {
-  type = map(object)
-
-  default = {
+  type = map(object({
+    vpc_name    = string
+    subnet_name = string
+    ip_range    = string
+  }))
+    default = {
     "vpc1" = {
-      vpc_name    = "sonu_vpc_1_object"
-      subnet_name = "sonu_subnet_1_object"
-      ip_range    = "10.0.0.0/24"
-    }
-  }
+    vpc_name    = "sonu-vpc-1-object"
+    subnet_name = "sonu-subne-1-object"
+    ip_range    = "10.0.0.0/24"
+}}
 }
 
 variable "virtual_machines" {
-  type = map(object)
+  type = map(object({
+    server_name  = string
+    machine_type = string
+    server_zone  = string
+    vpc_name     = string
+    static_ip    = string
+    subnet_name  = string
+  }))
+default = {
 
-  default = {
-    "vm1" = {
-      server_name = "sonu_vm_1_object"
-      machine_type = "e2-micro"
-      server_zone = "asia-east1-b"
-      vpc_name = "sonu_vpc_1_object"
-      static_ip = "10.0.0.1"
-      subnet_name = "sonu_subnet_1_object"
-    }
-  }
+  "vm1" = {
+    server_name  = "sonu-vm-1-object"
+    machine_type = "e2-micro"
+    server_zone  = "asia-east1-b"
+    vpc_name     = "sonu-vpc-1-object"
+    static_ip    = "10.0.0.1"
+    subnet_name  = "sonu-subnet-1-object"
+}
 }
 
+
+}
 
