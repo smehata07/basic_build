c1ba88821cee110eded7f10a206ab14112a0c8f9
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 13:34:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,6 +20,7 @@ subnet_name = each.value.subnet_name
 ip_range = each.value.ip_range
 
 }
+
 module "VM1" {
 source       = "github.com/smehata07/modules/tree/main/Virtual%20Machine"
 for_each     = var.virtual_machines
@@ -28,5 +29,5 @@ machine_type = each.value.machine_type
 zone         = each.value.server_zone
 network_name = each.value.vpc_name
 static_ip    = each.value.statics_ip
-subnet_name  = google_compute_subnetwork.subnet-apac-01.name
+subnet_name  = each.value.subnet_name
 }
