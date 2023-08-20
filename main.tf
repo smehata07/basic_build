7423936f5b2cf4f653dd71e103af18d69df7ae32
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 15:43:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,20 @@
 terraform {
-  required_providers{
-  google = {
-    
-    source  = "hashicorp/google"
-    version = " ~> 4.60.0"
-  }  
-  } 
+  required_providers {
+    google = {
+
+      source  = "hashicorp/google"
+      version = " ~> 4.60.0"
+    }
+  }
 }
 
 provider "google" {
-  project = "vm-deployment-apac"
-  region  = "asia-east1"
+  project     = "vm-deployment-apac"
+  region      = "asia-east1"
 }
+
 module "vpc" {
-  source      = "github.com/smehata07/modules/tree/main/VPC"
+  source      = "github.com/smehata07/modules.git//VPC"
   for_each    = var.vpc
   vpc_name    = each.value.vpc_name
   subnet_name = each.value.subnet_name
@@ -21,15 +22,14 @@ module "vpc" {
 }
 
 module "VM1" {
-  source       = "github.com/smehata07/modules/tree/main/Virtual%20Machine"
+  source       = "github.com/smehata07/modules.git//Virtual Machine"
   for_each     = var.virtual_machines
   server_name  = each.value.server_name
   machine_type = each.value.machine_type
   zone         = each.value.server_zone
   network_name = each.value.vpc_name
-  static_ip    = each.value.statics_ip
+  static_ip    = each.value.static_ip
   subnet_name  = each.value.subnet_name
 
   depends_on = [module.vpc]
 }
-
