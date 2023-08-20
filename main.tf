a208226c2029858297c325cf54098ce37748f15d
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 13:49:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,21 +13,23 @@ provider "google" {
   region  = "asia-east1"
 }
 module "vpc" {
-source = "github.com/smehata07/modules/tree/main/VPC"
-for_each = var.vpc
-vpc_name = each.value.vpc_name
-subnet_name = each.value.subnet_name
-ip_range = each.value.ip_range
-
+  source      = "github.com/smehata07/modules/tree/main/VPC"
+  for_each    = var.vpc
+  vpc_name    = each.value.vpc_name
+  subnet_name = each.value.subnet_name
+  ip_range    = each.value.ip_range
 }
 
 module "VM1" {
-source       = "github.com/smehata07/modules/tree/main/Virtual%20Machine"
-for_each     = var.virtual_machines
-server_name  = each.value.server_name
-machine_type = each.value.machine_type
-zone         = each.value.server_zone
-network_name = each.value.vpc_name
-static_ip    = each.value.statics_ip
-subnet_name  = each.value.subnet_name
+  source       = "github.com/smehata07/modules/tree/main/Virtual%20Machine"
+  for_each     = var.virtual_machines
+  server_name  = each.value.server_name
+  machine_type = each.value.machine_type
+  zone         = each.value.server_zone
+  network_name = each.value.vpc_name
+  static_ip    = each.value.statics_ip
+  subnet_name  = each.value.subnet_name
+
+  depends_on = [module.vpc]
 }
+
