5146080d9aab4b85638ce64147ea757c1ac067c0
smehata07 <109245697+smehata07@users.noreply.github.com>
2023-08-20 13:32:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,7 +12,21 @@ provider "google" {
   project = "vm-deployment-apac"
   region  = "asia-east1"
 }
+module "vpc" {
+source = "github.com/smehata07/modules/tree/main/VPC"
+for_each = var.vpc
+vpc_name = each.value.vpc_name
+subnet_name = each.value.subnet_name
+ip_range = each.value.ip_range
 
-resource "google_compute_network" "vpc-cloud-build" {
-  name                    = "vpc-through-cloud-build"
+}
+module "VM1" {
+source       = "github.com/smehata07/modules/tree/main/Virtual%20Machine"
+for_each     = var.virtual_machines
+server_name  = each.value.server_name
+machine_type = each.value.machine_type
+zone         = each.value.server_zone
+network_name = google_compute_network.vpc-apac-01.name
+static_ip    = each.value.statics_ip
+subnet_name  = google_compute_subnetwork.subnet-apac-01.name
 }
